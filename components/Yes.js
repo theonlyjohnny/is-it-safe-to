448dbc5fd3781e07070751569b99133cbf4dedf4
Johnny Dallas <theonlyjohnny@theonlyjohnny.sh>
2020-03-16 21:25:41 +0000

    add yes and no page components

---
+++ components/Yes.js
@@ -0,0 +1,24 @@
+function Yes(title, sentence) {
+  const choice = yesChoices[Math.random() * yesChoices.length];
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+        display: "flex"
+      }}
+    >
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={sentence}></meta>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1 style={{ margin: "auto" }}>{choice}</h1>
+    </div>
+  );
+}
+
+export default Yes;
